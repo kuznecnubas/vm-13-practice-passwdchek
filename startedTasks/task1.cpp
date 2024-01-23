c436fc6621455d289f7acb0ceae57a29d0bd9d8f
kuznecnubas <KuznechikTV@Yandex.kz>
2024-01-23 09:48:50 +0600

    1

--- startedTasks/task1.cpp
+++ startedTasks/task1.cpp
@@ -1,16 +1,50 @@
 #include <iostream>
 using namespace std;
 
-void checkPassword(string password){
+int checkLettersUpper(string password){
+    int letters = 0;
     for (int i = 0; i < password.length(); i++){
-        cout << password[i];
+        if (isupper(password[i])) letters++;
     }
+    return letters;
+}
+
+int checkLettersLower(string password){
+    int letters = 0;
+    for (int i = 0; i < password.length(); i++){
+        if (islower(password[i])) letters++;
+    }
+    return letters;
+}
+
+int checkDigits(string password){
+    int digits = 0;
+    for (int i = 0; i < password.length(); i++){
+        if (isdigit(password[i])) digits++;
+    }
+    return digits;
+}
+
+int checkSymbols(string password){
+    int symbols = 0;
+    for (int i = 0; i < password.length(); i++){
+        if (password[i] == '-' || password[i] == '!' || password[i] == '#' || password[i] == '$' ||
+            password[i] == '%' || password[i] == '&' || password[i] == '*' || password[i] == '@' ||
+            password[i] == '^') symbols++;
+    }
+    return symbols;
 }
 
 int main(){
     string password;
-    cout << "Проверка пароля" << endl;
-    cout << "Введите пароль: ";
-    getline(cin, password);
-    checkPassword(password);
+    while (true) {
+        cout << "\033[2J\033[1;1H"; //очистка
+        cout << "Проверка пароля\n" << endl;
+        if (checkLettersUpper(password) <= 0) cout << "Пароль должен содержать большие буквы (QWE)" << endl;
+        if (checkLettersLower(password) <= 0) cout << "Пароль должен содержать маленькие буквы (qwe)" << endl;
+        if (checkDigits(password) <= 0)cout << "Пароль должен содержать цифры (123)" << endl;
+        if (checkSymbols(password) <= 0)cout << "Пароль должен содержать символы (!@#)" << endl;
+        cout << "Введите пароль: ";
+        getline(cin, password);
+    }
 }
